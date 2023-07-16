ab2ca970fc0a810289c637758cfc24d8a14e6baf
pranavkondapaneni <pranavkondapaneni@gmail.com>
2023-07-16 15:59:06 -0500

    Created Files for Each List Element (Committed All)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { ListElementsComponent } from './list/list-elements/list-elements.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    ListElementsComponent
   ],
   imports: [
     BrowserModule,
