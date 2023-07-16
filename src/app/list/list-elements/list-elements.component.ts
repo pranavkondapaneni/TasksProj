bf81c7a14eeab8902e5bae3ddb94366041aea352
pranavkondapaneni <pranavkondapaneni@gmail.com>
2023-07-16 15:57:54 -0500

    Created Files for Each List Element

---
+++ src/app/list/list-elements/list-elements.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-elements',
+  templateUrl: './list-elements.component.html',
+  styleUrls: ['./list-elements.component.scss']
+})
+export class ListElementsComponent {
+
+}
